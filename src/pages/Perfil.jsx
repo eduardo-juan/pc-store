20b79e31623df2d78d7ae0677306bb0bf4a1f931
eduardo-juan <eduardo.juan.practica@gmail.com>
2026-09-23 14:40:49 -0600

    fix: reforzar campos obligatorios de perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -450,6 +450,7 @@ export default function Perfil() {
                 className="pc-input"
                 maxLength={50}
                 value={form.nombre}
+                required
                 onChange={(e) => cambiarCampo('nombre', e.target.value)}
                 style={estiloCampo('nombre')}
               />
@@ -461,6 +462,7 @@ export default function Perfil() {
                 className="pc-input"
                 maxLength={50}
                 value={form.apellido}
+                required
                 onChange={(e) => cambiarCampo('apellido', e.target.value)}
                 style={estiloCampo('apellido')}
               />
@@ -488,6 +490,7 @@ export default function Perfil() {
                 maxLength={9}
                 placeholder="9439-4343"
                 value={form.teléfono}
+                required
                 onChange={(e) => cambiarCampo('teléfono', e.target.value)}
                 style={estiloCampo('teléfono')}
               />
